49bc9e93b91a9f7aead4bb177407e0d057ebc8ab
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2023-06-05 01:18:08 +0200

    feat: update static files
    Signed-off-by: Kerim Bilgic <pfusch3d@gmail.com>

---
+++ assets/VerticalSpacer-03fc83f9.js
@@ -0,0 +1,1 @@
+import{$ as e,O as c,U as t}from"./index-1282da40.js";const r={},o={style:{height:"10px"}};function s(n,_){return c(),t("div",o)}const i=e(r,[["render",s]]);export{i as V};
--- assets/VerticalSpacer-a79fd279.js
+++
@@ -1,1 +0,0 @@
-import{$ as e,O as c,U as t}from"./index-0bf85db7.js";const r={},o={style:{height:"10px"}};function s(n,_){return c(),t("div",o)}const i=e(r,[["render",s]]);export{i as V};
